b3238db7feff27853806485ec007b01812b71312
KasunShanaka <kshanaka10@gmail.com>
2021-12-12 13:21:47 +0530

    filter Section

--- web/src/pages/ShoppingPage/ShoppingPage.js
+++ web/src/pages/ShoppingPage/ShoppingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './ShoppingPage.module.scss';
 
 import { useState } from 'react';
+import classnames from 'classnames';
 
 //components
 import Body from '../../shared/Body/Body';
@@ -11,6 +12,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import NativeSelect from '@mui/material/NativeSelect';
 
 const ShoppingPage = () => {
 
@@ -29,28 +31,30 @@ const ShoppingPage = () => {
                 <div className={style.body}>
                     <Sidebar />
                     <div className={style.contents}>
-                        <div className={style.contentHeader}>
+                        <div className={classnames(style.contentHeader, style.gap)}>
                             All Toys
                         </div>
                         <div className={style.filterSection}>
-                            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-                                <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-standard-label"
-                                    id="demo-simple-select-standard"
-                                    value={age}
-                                    onChange={handleChange}
-                                    label="Age"
-                                    size='small'
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
+                            <div className={style.selectBox}>
+                                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                                    <InputLabel id="demo-simple-select-standard-label">Sort by</InputLabel>
+                                    <Select
+                                        // defaultValue={10}
+                                        labelId="demo-simple-select-standard-label"
+                                        id="demo-simple-select-standard"
+                                        value={age}
+                                        onChange={handleChange}
+                                        label="Age"
+                                    >
+                                        {/* <MenuItem value="">
+                                            <em>None</em>
+                                        </MenuItem> */}
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
                         </div>
                     </div>
                 </div>
