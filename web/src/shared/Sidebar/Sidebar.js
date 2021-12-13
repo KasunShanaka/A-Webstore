48ac65a052d41cc829e45999ecb496b162402960
KasunShanaka <kshanaka10@gmail.com>
2021-12-13 17:45:06 +0530

    Start Sidebar

--- web/src/shared/Sidebar/Sidebar.js
+++ web/src/shared/Sidebar/Sidebar.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import style from './Sidebar.module.scss';
 
+//components
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+
 const Sidebar = () => {
+
+    const [open, setOpen] = React.useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+
     return (
         <div className={style.container}>
-            
+            <ListItemButton onClick={handleClick}>
+                Inbox
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItemButton >
+                        Inside
+                    </ListItemButton>
+                </List>
+            </Collapse>
+            <ListItemButton onClick={handleClick}>
+                Inbox
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItemButton >
+                        Inside
+                    </ListItemButton>
+                </List>
+            </Collapse>
         </div>
     )
 }
