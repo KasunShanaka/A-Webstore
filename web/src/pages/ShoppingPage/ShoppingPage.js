f63360b569f59cd4665323a95a44f69132fdc575
KasunShanaka <kshanaka10@gmail.com>
2021-12-13 17:29:41 +0530

    Shopping page Finished

--- web/src/pages/ShoppingPage/ShoppingPage.js
+++ web/src/pages/ShoppingPage/ShoppingPage.js
@@ -13,6 +13,13 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 // import NativeSelect from '@mui/material/NativeSelect';
+import Pagination from '@mui/material/Pagination';
+
+//icons
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 
 const ShoppingPage = () => {
 
@@ -61,27 +68,211 @@ const ShoppingPage = () => {
                                 </div>
                             </div>
                             <div className={style.productsSection}>
-                                <div className={style.leftspace}>
-                                </div>
                                 <div className={style.products}>
                                     <div className={style.product}>
                                         <div className={style.productImg}>
                                             <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
                                         </div>
-                                        <div className={style.pName}>
-                                            1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
-                                        </div>
-                                        <div className={style.pCategory}>
-                                            Puzzle
-                                        </div>
-                                        <div className={style.pPrice}>
-                                            $19.78
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
                                         </div>
                                     </div>
-                                    
+                                    <div className={style.product}>
+                                        <div className={style.productImg}>
+                                            <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
+                                        </div>
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={style.product}>
+                                        <div className={style.productImg}>
+                                            <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
+                                        </div>
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={style.product}>
+                                        <div className={style.productImg}>
+                                            <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
+                                        </div>
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={style.product}>
+                                        <div className={style.productImg}>
+                                            <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
+                                        </div>
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={style.product}>
+                                        <div className={style.productImg}>
+                                            <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
+                                        </div>
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={style.product}>
+                                        <div className={style.productImg}>
+                                            <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
+                                        </div>
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={style.product}>
+                                        <div className={style.productImg}>
+                                            <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
+                                        </div>
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={style.product}>
+                                        <div className={style.productImg}>
+                                            <img src="https://img.etimg.com/thumb/msid-81643604,width-1440,height-1080,imgsize-151435,resizemode-8,quality-100/small-biz/sme-sector/no-kidding-india-gears-up-to-create-its-own-toy-story.jpg" alt="" />
+                                        </div>
+                                        <div className={style.productInfo}>
+                                            <div className={style.pName}>
+                                                1200 pieces of jigsaw puzzle Lorem ipsum dolor sit amet.
+                                            </div>
+                                            <div className={style.pCategory}>
+                                                Puzzle
+                                            </div>
+                                            <div className={style.pPrice}>
+                                                $19.78
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
-                                <div className={style.rightspace}>
+                                <div className={style.pagination}>
+                                    <Pagination count={5} size="small" />
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.footer}>
+                        <div className={style.customerService}>
+                            <div className={style.footerHeader}>
+                                Customer Service
+                            </div>
+                            <div className={style.links}>
+                                <div className={style.aboutLink}>
+                                    About
+                                </div>
+                                <div className={style.contactLink}>
+                                    Contact
+                                </div>
+                                <div className={style.newsLink}>
+                                    News and Events
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.visitOurStore}>
+                            <div className={style.footerHeaderStore}>
+                                Visit Our store
+                            </div>
+                            <div className={style.storeInfo}>
+                                <div className={style.Link}>
+                                    45 Kenry Road, <br />
+                                    San Francisco, CA <br />
+                                    (415) 291 763 242
+                                </div>
+                                <br />
+                                <div className={style.openDays}>
+                                    Monday - Friday 9:00 - 5:00 <br />
+                                    Weekends 10:00 - 3:00
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.connectWithUs}>
+                            <div className={style.footerHeaderContact}>
+                                Contact with us
+                            </div>
+                            <div className={style.socialMedia}>
+                                <span className={style.instagram}>
+                                    <InstagramIcon></InstagramIcon>
+                                </span>
+                                <span className={style.facebook}>
+                                   <FacebookIcon
+                                   ></FacebookIcon>
+                                </span>
+                                <span className={style.twitter}>
+                                    <TwitterIcon></TwitterIcon>
+                                </span>
+                                <span className={style.pinterest}>
+                                    <PinterestIcon></PinterestIcon>
+                                </span>
                             </div>
                         </div>
                     </div>
