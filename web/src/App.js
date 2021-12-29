6eb95651bca47232639527c86c056f5eb820dd62
KasunShanaka <kshanaka10@gmail.com>
2021-12-29 13:04:53 +0530

    Created Paths for the CartSummary Page

--- web/src/App.js
+++ web/src/App.js
@@ -5,11 +5,15 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 //Pages
 import ShoppingPage from './pages/ShoppingPage/ShoppingPage';
 import ProductPage from './pages/ProductPage/ProductPage';
+import CartSummary from './pages/CartSummary/CartSummary';
 
 function App() {
 
   const theme = createTheme({
     palette: {
+      common: {
+        white: '#FFFAFA'
+      },
       primary: {
         main: '#ff7700'
       }
@@ -28,6 +32,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<ShoppingPage />} />
           <Route exact path="/Product" element={<ProductPage />} />
+          <Route exact path="/CartSummary" element={<CartSummary />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
