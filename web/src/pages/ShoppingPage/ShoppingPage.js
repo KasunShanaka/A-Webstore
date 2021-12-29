7a198ca16fea78a2e440cbf535af860591a7a121
KasunShanaka <kshanaka10@gmail.com>
2021-12-29 13:05:24 +0530

    Moving Footer to a  shared component

--- web/src/pages/ShoppingPage/ShoppingPage.js
+++ web/src/pages/ShoppingPage/ShoppingPage.js
@@ -8,6 +8,7 @@ import classnames from 'classnames';
 import Body from '../../shared/Body/Body';
 import Sidebar from '../../shared/Sidebar/Sidebar';
 import Navbar from '../../shared/Navbar/Navbar';
+import Footer from '../../shared/Footer/Footer';
 
 //Material UI components
 // import NativeSelect from '@mui/material/NativeSelect';
@@ -20,11 +21,6 @@ import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 
-//icons
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import PinterestIcon from '@mui/icons-material/Pinterest';
 
 const ShoppingPage = () => {
 
@@ -242,61 +238,7 @@ const ShoppingPage = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={style.footer}>
-                        <div className={style.customerService}>
-                            <div className={style.footerHeader}>
-                                Customer Service
-                            </div>
-                            <div className={style.links}>
-                                <div className={style.aboutLink}>
-                                    About
-                                </div>
-                                <div className={style.contactLink}>
-                                    Contact
-                                </div>
-                                <div className={style.newsLink}>
-                                    News and Events
-                                </div>
-                            </div>
-                        </div>
-                        <div className={style.visitOurStore}>
-                            <div className={style.footerHeaderStore}>
-                                Visit Our store
-                            </div>
-                            <div className={style.storeInfo}>
-                                <div className={style.Link}>
-                                    45 Kenry Road, <br />
-                                    San Francisco, CA <br />
-                                    (415) 291 763 242
-                                </div>
-                                <br />
-                                <div className={style.openDays}>
-                                    Monday - Friday 9:00 - 5:00 <br />
-                                    Weekends 10:00 - 3:00
-                                </div>
-                            </div>
-                        </div>
-                        <div className={style.connectWithUs}>
-                            <div className={style.footerHeaderContact}>
-                                Contact with us
-                            </div>
-                            <div className={style.socialMedia}>
-                                <span className={style.instagram}>
-                                    <InstagramIcon></InstagramIcon>
-                                </span>
-                                <span className={style.facebook}>
-                                    <FacebookIcon
-                                    ></FacebookIcon>
-                                </span>
-                                <span className={style.twitter}>
-                                    <TwitterIcon></TwitterIcon>
-                                </span>
-                                <span className={style.pinterest}>
-                                    <PinterestIcon/>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
+                    <Footer/>
                 </div>
                 <div className={style.rightSpace}>
 
